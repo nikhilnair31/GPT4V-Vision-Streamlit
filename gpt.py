de0280fc4847b0868c075db7a9d05a5498bc0f32
Nikhil Nair <niknair31898@gmail.com>
2023-11-30 14:16:44 -0600

    Init

---
+++ gpt.py
@@ -0,0 +1,44 @@
+import openai
+import base64
+from PIL import Image
+import io
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+class OpenAIGPT:
+    def __init__(self, OPENAI_API_KEY):
+        self.client = openai.Completion()
+        openai.api_key = OPENAI_API_KEY
+
+    def get_completion(self, system_prompt, image_path):
+        with open(image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        
+        response = self.client.create(
+            model="gpt-4-vision-preview",
+            messages=[
+                {
+                    "role": "system",
+                    "content": [
+                        {"type": "text", "text": system_prompt},
+                    ],
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{encoded_string}",
+                                "detail": "low"
+                            },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=100,
+        )
+        return response.choices[0].message.content
+
+# Remember to replace 'your_api_key_here' with your actual OpenAI API key
+gpt_interface = OpenAIGPT(OPENAI_API_KEY=OPENAI_API_KEY)
---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from gpt import OpenAIGPT  # this assumes your class is in a file named streamlit_openai_interface.py
+
+st.title('Image Interpretation with GPT-4 Vision')
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+system_prompt = st.text_input("System prompt", "Please describe this image.")
+
+# Initialize GPT-4 interface with your API key
+gpt_interface = OpenAIGPT(openai_api_key='your_api_key_here')
+
+if uploaded_file is not None:
+    image_bytes = uploaded_file.getvalue()
+    
+    # Once we have an image, display it
+    st.image(image_bytes, caption='Uploaded Image')
+
+    if st.button('Interpret Image'):
+        with st.spinner(text='Interpreting the image...'):
+            response = gpt_interface.get_completion(system_prompt, image_bytes)
+            st.text(response)
