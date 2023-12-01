082195afa53afba914dbb0c5bc661f8a2888b2b2
Nikhil Nair <niknair31898@gmail.com>
2023-11-30 18:46:22 -0600

    Fixed uplaod

--- gpt.py
+++ gpt.py
@@ -9,7 +9,7 @@ class OpenAIGPT:
     def __init__(self, openai_api_key):
         self.client = OpenAI(api_key=openai_api_key)
 
-    def get_completion(self, system_prompt, image_path):
+    def get_completion(self, system_prompt, image_file):
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         
         response = self.client.chat.completions.create(
